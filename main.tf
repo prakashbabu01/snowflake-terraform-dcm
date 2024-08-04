b96fcc47bf86169cd9614a72d973f7e1fd33b616
Prakash Posam <prakashbabu01@gmail.com>
2024-08-04 19:42:57 -0400

    updated database details

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+
+  backend "remote" {
+    organization = "snowflake-prod"
+
+    workspaces {
+      name = "snowflake-prod-workspace"
+    }
+  }
+
+}
+
+provider "snowflake" {
+  role = "SYSADMIN"
+}
+
+resource "snowflake_database" "mysnowdb" {
+  name    = "mysnowdatabase"
+  comment = "snowflake database using terraform"
+}
